85c9190fa5dca450121e96aae5545315513dcac2
kidow <wcgo2ling@gmail.com>
2021-06-19 12:57:23 +0900

    font render hinting none

--- utils/index.ts
+++ utils/index.ts
@@ -19,7 +19,7 @@ const getOptions = async (): Promise<ChromeOptions> => {
   }
   if (!isDev) {
     options = {
-      args: [...chrome.args, '--font-render-hinting=medium'],
+      args: [...chrome.args, '--font-render-hinting=none'],
       executablePath: await chrome.executablePath,
       headless: true
     }
