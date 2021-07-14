ad5a6d4cde920c9f9411307d7683748c14ed7b2b
kidow <wcgo2ling@gmail.com>
2021-07-14 19:31:51 +0900

    image loader 추가

--- components/CopyImage/index.tsx
+++ components/CopyImage/index.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import classnames from 'classnames'
 import { useToast } from 'services'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { ClipLoader } from 'react-spinners'
 
 interface Props {
   loading: boolean
@@ -13,13 +15,17 @@ interface State {}
 const ReCopyImage: FunctionComponent<Props> = ({ loading, url, onLoad }) => {
   const toast = useToast()
   return (
-    <div className="mb-4 cursor-pointer">
+    <div className="mb-4 cursor-pointer relative">
+      <div className="absolute z-10 left-1/2 top-1/2 flex items-center justify-center -ml-6 -mt-6">
+        <ClipLoader size={48} loading={loading} color="rgb(209, 213, 219)" />
+      </div>
       <CopyToClipboard
         text={encodeURI(url)}
         onCopy={() => toast.success('이미지 URL이 복사되었습니다.')}
       >
-        <img
+        <LazyLoadImage
           src={url}
+          effect="blur"
           alt="sign"
           title="클릭해서 이미지 주소를 복사하세요."
           onLoad={onLoad}
